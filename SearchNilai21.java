1fde5f6987ff702d847822ac7284bf835eb002e4
nevita <triyanevita7@gmail.com>
2024-10-31 23:01:52 +0700

    percobaan 4

--- SearchNilai21.java
+++ SearchNilai21.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
 public class SearchNilai21 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int indeks = -1;
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
+        System.out.print("Masukkan nilai yang akan diinput: ");
+        int nilai = sc.nextInt();
+
+        int[] nilaiMhs = new int [nilai];
+       
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int nilaiDicari = sc.nextInt();
+
+        int indeks = -1;
+        for (int i = 1; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] == nilaiDicari) {
                 indeks = i;
                 break;
             }
         }
         if (indeks != -1) {
-            System.out.println("Nilai " + key + " ketemu di indeks ke-" + indeks);
+            System.out.println("Nilai " + nilaiDicari + " ketemu, merupakan nilai mahasiswa ke-" + (indeks+1));
         }else {
-            System.out.println("Nilai " + key + " tidak ditemukan" );
+            System.out.println("Nilai " + nilaiDicari + " tidak ditemukan" );
         }
     }
     
