f52a9c11d7b08f9e0793c83c7e475e7f31cdf2ae
nevita <triyanevita7@gmail.com>
2024-10-31 17:38:45 +0700

    percobaan 2

--- ArrayNilai21.java
+++ ArrayNilai21.java
@@ -4,14 +4,18 @@ public class ArrayNilai21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiAkhir = new int[10];
+        int [] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();   
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);      
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");   
+            } else if (nilaiAkhir[i] < 70) {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!") ;
+            }
         }
     }  
 }
