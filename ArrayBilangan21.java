b38219d7a91175dee166ce0b7b2ae71764ad2d2f
nevita <triyanevita7@gmail.com>
2024-10-31 15:54:52 +0700

    percobaan 1

--- ArrayBilangan21.java
+++ ArrayBilangan21.java
@@ -3,16 +3,16 @@ public class ArrayBilangan21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [] bil = new int[4];
+        int [] bil;
+        bil = new int[4];
+        
         bil [0] = 5;
         bil [1] = 13;
         bil [2] = -7;
         bil [3] = 17;
 
-        System.out.println(bil [0]);
-        System.out.println(bil [1]);
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
-
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil [i]);   
+        }
     }
 }
